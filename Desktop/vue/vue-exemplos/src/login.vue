07f9ee683d7be6db9b0dc551c95a1d4cabdf5e00
taeltico <rafaelwvs16@gmaail.com>
2022-03-23 18:52:41 -0300

    atualização paginas para vue

---
+++ Desktop/vue/vue-exemplos/src/login.vue
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+</html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Escambo</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/padrao.css">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="css/cores.css">
+</head>
+
+<body class="body"><br>
+
+    <div class="login">
+        <form action="processa.php" class="col-12 col-md-6 m-auto text-center">
+            <br>
+            <h2>Faça seu login</h2><br> E-mail:
+
+            <br>
+            <input type="Email" name="login"><br> Senha:
+
+            <br>
+            <input type="password" name="senha"><br>
+
+            <input type="submit" value="Logar"><br>
+            <a href="RecuperarSenha.html"> Recuperar Senha</a><br> Ainda não tem uma conta? <a href="cadastro.html">Cadastre-se </a><br>
+
+        </form><br>
+    </div>
+</body>
+
+</html>
